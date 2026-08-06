fc014d1e54e3c5d752ee2182210b741e747222f6
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 12:41:20 +0700

    feat: add FizzBuzz (Ruby)

---
+++ programs/fizzbuzz.rb
@@ -0,0 +1,14 @@
+# fizzbuzz.rb
+# Cetak 1-100 dengan aturan FizzBuzz.
+
+(1..100).each do |i|
+  if i % 15 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i
+  end
+end
